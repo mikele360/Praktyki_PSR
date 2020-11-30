636445f6b5ed9802d7a061b04579aa71498db3c4
mikele360 <minikosu@gmail.com>
2020-11-30 14:36:36 +0100

    nowy

---
+++ src/Cwiczenia/Funkcje1.java
@@ -0,0 +1,50 @@
+package Cwiczenia;
+
+public class Funkcje1 {
+
+    public static int rekur(int x) {
+
+        if (x > 0) {
+            return rekur(x - 1);
+        }
+        return x;
+    }
+
+    public static int fibb(int x) {
+        if (x == 0) return 0;
+        if (x == 1) return 1;
+        if (x > 1)
+            return fibb(x - 1) + fibb(x - 2);
+        else {
+             return 0;
+        }
+    }
+
+
+
+    public static float funkcja_kwadratowa(float x) {
+
+        return 2 * x * x - 3 * x + 4;
+    }
+
+    public static void print_table(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(funkcja_kwadratowa(5));
+//        int[] tab = {1, 4, 6, 8, 9, 3, 6, 9, 3, 7, 9, 3};
+//        print_table(tab);
+
+        long time = System.currentTimeMillis();
+
+        System.out.println(fibb(9));
+    }
+
+
+}
+
+
--- src/Cwiczenia/zadanie_1.java
+++ src/Cwiczenia/zadanie_1.java
@@ -2,11 +2,10 @@ package Cwiczenia;
 
 public class zadanie_1 {
     public static void main(String[] args) {
-        System.out.println("Pętle w Javie");
-        System.out.println("Pętla for");
+
         int a=0;
 
-        System.out.println("Pętla while");
+
         a=0;
         while((a++)<20){
             System.out.println("Liczba "+ a +" jest "+(a%2==0?"parzyste":"nieparzyste"));
